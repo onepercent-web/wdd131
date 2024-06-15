ed8d4b1ff014881855d88eb283199525f6a32071
onepercent-web <keyami2019@gmail.com>
2024-06-16 03:28:00 +0900

    Save in file

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    document.getElementById('currentyear').textContent = new Date().getFullYear();
+    document.getElementById('lastModified').textContent = 'Last modified: ' + document.lastModified;
+};
